9e04b014d29246b7ff88277ffa88301fb140464d
Uj-K <umkrall@gmail.com>
2024-12-05 16:38:06 -0800

    Added Google font

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ## My Favorite Restaurants in Tacoma Area
-Using [Leaflet.js](https://leafletjs.com/) library, display my favorite restaurant on a map and provide brief information about each restaurant.
+Using [Leaflet.js](https://leafletjs.com/) and [Bootstrap](https://getbootstrap.com/) library, display my favorite restaurant on a map and provide brief information about each restaurant.
--- main.js
+++ main.js
@@ -10,61 +10,51 @@ var restaurants = [
         name: "BURGER SEOUL",
         lat: 47.25687308003812,
         lng: -122.4615809305217,
-        description: "Hamburger restaurant"
     },
     {
         name: "Cuerno Bravo",
         lat: 47.2582716481405, 
         lng: -122.44299593620235,
-        description: "Steakhouse"
     },
     {
         name: "Outpost Sandwiches",
         lat: 47.25369974049884,
         lng: -122.43727585264476,
-        description: "Sandwich shop"
     },
     {
         name: "KIMCHI BOX",
         lat: 47.233645026792736, 
         lng: -122.5035077446855,
-        description: "Korean"
     },
     {
         name: "Wooden City Tacoma",
         lat: 47.256876343657254, 
         lng: -122.43962678760512,
-        description: "Casual dining"
     },
     {
         name: "The Grill",
         lat: 47.17266585857368, 
         lng: -122.48428232256246,
-        description: "Korean"
     },
     {
         name: "Salamone’s Pizza",
         lat: 47.265355619446396, 
         lng: -122.4474922807633,
-        description: "Pizza"
     },
     {
         name: "Field Bar & Bottle Shop",
         lat: 47.25552427471303, 
         lng: -122.47090430419273,
-        description: "Unique cuisine"
     },
     {
         name: "Red Gem",
         lat: 47.19838979354568, 
         lng: -122.48312168045177,
-        description: "Korean-Chinese restaurant·"
     },
     {
         name: "bb.q Chicken Tacoma Lakewood",
         lat: 47.17835205233487, 
         lng: -122.48470033179389,
-        description: "Korean Fried Chicken"
     }
 
 ];
@@ -76,6 +66,6 @@ var myIcon = L.icon({
 
 restaurants.forEach(function(restaurant) {
     var marker = L.marker([restaurant.lat, restaurant.lng], {icon: myIcon}).addTo(map);
-    marker.bindPopup("<b>" + restaurant.name + "</b><br>" + restaurant.description);
+    marker.bindPopup("<b>" + restaurant.name + "</b><br>");
 });
 
